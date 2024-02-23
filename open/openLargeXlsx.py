8fdf171d71732195f02d29c48742a89cc9e25ef9
Martinez Ortiz <fabiola.martinez.ortiz@sap.com>
2024-02-23 10:16:39 -0600

    u: working

--- open/openLargeXlsx.py
+++ open/openLargeXlsx.py
@@ -1,10 +1,20 @@
+"""
+Open an Excel file, reads an speciic sheet and writes its info on a new xlsx file
+"""
 import pandas as pd
+import traceback
+import pickle
 
 save_path = "C:/Users/I746992/Desktop/Remove_Columns_Python/"
 # read sheet of an excel file
-file = "C:/Users/I746992/OneDrive - SAP SE/General - Data Intelligence LAC/1. Mexico/2. Target List/3. Target Lists - Reports/MasterFile- MX.xlsx"
-#file = "C:/Users/I746992/OneDrive - SAP SE/General - Data Intelligence LAC/LOB-PE_ID-PE_name.xlsx"
-df = pd.read_excel(file, sheet_name="Efectivas") 
-df.to_pickle(save_path + "dummyEfectivas.pkl", compression='zip')
-#df.to_excel(save_path + "efectivas.xlsx", index = False)
-# print(dataframe1)
+file = "C:/Users/I746992/Desktop/MasterFile- MX.xlsx"
+
+try:
+    f = open(file, 'r')  
+    df = pd.read_excel(file, sheet_name="EFECTIVAS")
+except Exception as error:
+    print(error)
+    traceback.print_exc()
+#df.to_pickle(save_path + "dummyEfectivas.pkl", compression='zip') 
+#file_bytestream = bytes()
+pickle.dump(df, f)
--- open/openXlsxBook.py
+++ open/openXlsxBook.py
@@ -2,11 +2,19 @@
 Open an Excel file, reads an speciic sheet and writes its info on a new xlsx file
 """
 import pandas as pd
-
+# TEST
 save_path = "C:/Users/I746992/Desktop/Remove_Columns_Python/"
+# booksheet_name = "Sheet1"
+# MASTER FILE
+#save_path = "C:/Users/I746992/Desktop/MasterFile- MX.xlsx"
+booksheet_name = "BASE"
 # read sheet of an excel file
+# TEST
+#file = "C:/Users/I746992/OneDrive - SAP SE/General - Data Intelligence LAC/LOB-PE_ID-PE_name.xlsx"
 #file = "C:/Users/I746992/OneDrive - SAP SE/General - Data Intelligence LAC/1. Mexico/2. Target List/3. Target Lists - Reports/MasterFile- MX.xlsx"
-file = "C:/Users/I746992/OneDrive - SAP SE/General - Data Intelligence LAC/LOB-PE_ID-PE_name.xlsx"
-df = pd.read_excel(file, sheet_name="Sheet1") 
+# MASTER FILE
+file = "C:/Users/I746992/Desktop/MasterFile- MX.xlsx"
+df = pd.read_excel(file, sheet_name='EFECTIVAS') 
+print(list(df.keys()))
 #df.to_pickle(save_path + "dummyEfectivas.pkl", compression='zip')
 df.to_excel(save_path + "lob_test.xlsx", index = False)
